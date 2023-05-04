281cda18e4d3a69c07bb091ed9c412c5343cf130
Nikita <nik.shadkov@gmail.com>
2023-05-04 15:08:36 +0300

    We've added some new text!

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("I'm Nikita");
+        System.out.println("And I am programmaer!");
     }
 }
